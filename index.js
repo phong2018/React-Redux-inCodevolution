0fb90409b14e6c5df556fb6d42acdac2ebce377f
githubplg@1 <phong2018@gmail.com>
2021-06-19 18:56:50 +0700

    add: action and reducer BUY_CAKE

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log('idex.js')
+const BUY_CAKE= 'BUY_CAKE'
+
+function buyCake(){
+    return {
+        type:BUY_CAKE,
+        info: 'Firs redux action'
+    }
+}
+
+// (previousState, action)=>newState
+
+const initalState = {
+    numberOfCakes: 10
+}
+
+const reducer = (state = initalState, action) => {
+    switch (action.type) {
+        case BUY_CAKE: return {
+            ... state,
+            numberOfCakes: state.numberOfCakes - 1
+        }
+
+        default: return state
+    }
+}
+
+
